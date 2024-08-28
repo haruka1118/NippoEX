bf5b505c1f9c2de26ec22c7014064d2744244d88
haruka1118 <kogeteru@gmail.com>
2024-08-28 11:10:25 +0900

    urlを別ファイルで管理完了

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from apscheduler.schedulers.background import BackgroundScheduler
 from peewee import Model, CharField, DateTimeField, SqliteDatabase
 import datetime
+from urls import urls
 
 # SQLiteデータベースの設定
 db = SqliteDatabase("articles.db")
@@ -28,30 +29,6 @@ db.create_tables([ArticleHash])
 
 app = Flask(__name__)
 
-# ニュース記事のURLリスト
-urls = {
-    "県内ニュース": [
-        ("主要", "https://www.iwate-np.co.jp/article/category/main"),
-        ("経済", "https://www.iwate-np.co.jp/article/category/economy"),
-        ("医療・福祉", "https://www.iwate-np.co.jp/article/category/medical"),
-        ("行政", "https://www.iwate-np.co.jp/article/category/politics"),
-        ("教育", "https://www.iwate-np.co.jp/article/category/education"),
-        ("選挙", "https://www.iwate-np.co.jp/article/category/election"),
-        ("エンタメ", "https://www.iwate-np.co.jp/article/category/entertainment"),
-    ],
-    "全国ニュース": [
-        ("主要", "https://www.iwate-np.co.jp/article/kyodo/category/main"),
-        ("社会", "https://www.iwate-np.co.jp/article/kyodo/category/national"),
-        ("政治", "https://www.iwate-np.co.jp/article/kyodo/category/politics"),
-        ("経済", "https://www.iwate-np.co.jp/article/kyodo/category/economics"),
-        ("国際", "https://www.iwate-np.co.jp/article/kyodo/category/world"),
-        ("スポーツ", "https://www.iwate-np.co.jp/kyodo/category/sports"),
-        ("エンタメ", "https://www.iwate-np.co.jp/article/oricon"),
-        ("暮らし・話題", "https://www.iwate-np.co.jp/article/kyodo/category/lifestyle"),
-        ("文化・芸能", "https://www.iwate-np.co.jp/article/kyodo/category/cluture"),
-        ("科学・環境・医療・健康", "https://www.iwate-np.co.jp/article/kyodo/category/science"),
-    ],
-}
 
 # LINE Notifyのトークン
 LINE_NOTIFY_TOKEN = os.getenv("LINE_NOTIFY_TOKEN")
---
+++ urls.py
@@ -0,0 +1,24 @@
+# ニュース記事のURLリスト
+urls = {
+    "県内ニュース": [
+        ("主要", "https://www.iwate-np.co.jp/article/category/main"),
+        ("経済", "https://www.iwate-np.co.jp/article/category/economy"),
+        ("医療・福祉", "https://www.iwate-np.co.jp/article/category/medical"),
+        ("行政", "https://www.iwate-np.co.jp/article/category/politics"),
+        ("教育", "https://www.iwate-np.co.jp/article/category/education"),
+        ("選挙", "https://www.iwate-np.co.jp/article/category/election"),
+        ("エンタメ", "https://www.iwate-np.co.jp/article/category/entertainment"),
+    ],
+    "全国ニュース": [
+        ("主要", "https://www.iwate-np.co.jp/article/kyodo/category/main"),
+        ("社会", "https://www.iwate-np.co.jp/article/kyodo/category/national"),
+        ("政治", "https://www.iwate-np.co.jp/article/kyodo/category/politics"),
+        ("経済", "https://www.iwate-np.co.jp/article/kyodo/category/economics"),
+        ("国際", "https://www.iwate-np.co.jp/article/kyodo/category/world"),
+        ("スポーツ", "https://www.iwate-np.co.jp/kyodo/category/sports"),
+        ("エンタメ", "https://www.iwate-np.co.jp/article/oricon"),
+        ("暮らし・話題", "https://www.iwate-np.co.jp/article/kyodo/category/lifestyle"),
+        ("文化・芸能", "https://www.iwate-np.co.jp/article/kyodo/category/cluture"),
+        ("科学・環境・医療・健康", "https://www.iwate-np.co.jp/article/kyodo/category/science"),
+    ],
+}
