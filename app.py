204aee43c88d0011f2b2e567b4bb8d985c143af3
haruka1118 <kogeteru@gmail.com>
2024-09-02 08:21:14 +0900

    UptimeRobot使用開始(コードは変更なし)

--- app.py
+++ app.py
@@ -182,7 +182,3 @@ def index():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))  # dotenvにportがあればそれ、なければ5000
     app.run(host="0.0.0.0", port=port)  # どこからでもこのプログラムにアクセスできるように
-
-# 定期スケジュールで記事の情報もDBにいれる
-# DBからLPにだす
-# LPは降順(記事の取得日時)
