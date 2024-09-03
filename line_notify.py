fdd73138461a4099b7c3a4b1e6fabd5f1d1d9904
haruka1118 <kogeteru@gmail.com>
2024-09-03 14:39:06 +0900

    image削除

--- line_notify.py
+++ line_notify.py
@@ -8,15 +8,12 @@ load_dotenv()
 LINE_NOTIFY_TOKEN = os.getenv("LINE_NOTIFY_TOKEN")
 
 
-def send_line_notify(message, image_url=None):
+def send_line_notify(message):
     headers = {
         "Authorization": f"Bearer {LINE_NOTIFY_TOKEN}",
         "Content-Type": "application/x-www-form-urlencoded",
     }
     line_message = {"message": message}
-    # if image_url:
-    #     line_message["imageThumbnail"] = image_url
-    #     line_message["imageFullsize"] = image_url
 
     response = requests.post("https://notify-api.line.me/api/notify", headers=headers, data=line_message)
     return response
